a5feaa6568441fe52ec9e411a4e2446d1ce135fe
Peter Lange <peter.loliveira@gmail.com>
2020-02-05 10:34:59 -0300

    Aulas JavaScript

--- js/comandos.js
+++ js/comandos.js
@@ -1,14 +1,21 @@
-alert("OI vcs")
+// alert("OI vcs")
 
-console.log("Oi no Console")
+// console.log("Oi no Console")
 
-var a = 10, b = 20
-console.log(a + b)
+// var a = 10, b = 20
+// console.log(a + b)
 
-function soma(c, d) {
-    return c + d;
+// function soma(c, d) {
+//     return c + d;
+// }
+
+// a = 20
+// b = 40
+// console.log(soma(a,b))
+
+document.getElementById("btn-calcular").onclick = function(){
+    // var valorA = document.getElementById("valor-a").value
+    // var valorB = document.getElementById("valor-b").value    
+    alert(parseInt(document.getElementById("valor-a").value) + parseInt(document.getElementById("valor-b").value))
+    
 }
-
-a = 20
-b = 40
-console.log(soma(a,b))
